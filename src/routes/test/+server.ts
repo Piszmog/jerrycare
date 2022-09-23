5159e603f9535a42acdf0ef39068e59b38c2c253
Randell <piszmogcode@gmail.com>
2022-09-22 19:13:48 -0600

    Remove unused pages

--- src/routes/test/+server.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestEvent, RequestHandler } from './$types';
-import { getAnimal, setCompleteSteps } from '$lib/utils/prisma';
-
-export const GET: RequestHandler = async ({ params }: RequestEvent) => {
-  const a = await getAnimal(1);
-  return new Response();
-};
